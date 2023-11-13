a04c83955aa02053b8101c4fa16ece6569ec4403
Nguyen Thu Thao <ntthuthao1121@gmail.com>
2023-11-13 21:59:22 +0700

    feat: [B24] Notification entity

---
+++ Model/Entities/Notification.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BKConnectBE.Model.Entities
+{
+    [Table("Notifications")]
+    public class Notification
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public string Type { get; set; }
+
+        public string Avatar { get; set; }
+        
+        public string ContentId { get; set; }
+    }
+}
